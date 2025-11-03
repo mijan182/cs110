435dabca3f3d0c846519b29dcf1f12a4038fef54
mijan182 <jana-marie.reyes@rcsd.ca>
2025-11-03 11:21:36 -0600

    meow

--- labquestions/nov3/ex3_stars.cpp
+++ labquestions/nov3/ex3_stars.cpp
@@ -11,20 +11,19 @@ using namespace std;
 int main() {
     int numStars;
     
-    // Get user input for number of stars on first line
+    //get user input for number of stars on first line
     cout << "How many stars on the first line? ";
     cin >> numStars;
     cout << endl;
 
-    // Outer loop: controls the number of rows (counting down from numStars to 1)
+    //for loop: controls the number of rows counting down from 1
     for (int row = numStars; row >= 1; row--) {
         
-        // Inner loop: prints stars for current row
+        //2nd for loop: prints stars for current row
         for (int starCount = 1; starCount <= row; starCount++) {
             cout << "*";
         }
-        
-        // Move to next line after printing all stars for current row
+        //goes to next line
         cout << endl;
     }
     
