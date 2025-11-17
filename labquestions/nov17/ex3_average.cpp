4b89fe55860c210f45f84a0f2772f56921e47a32
mijan182 <jana-marie.reyes@rcsd.ca>
2025-11-17 11:57:19 -0600

    updated codefor ex3 nov17

--- labquestions/nov17/ex3_average.cpp
+++ labquestions/nov17/ex3_average.cpp
@@ -8,12 +8,7 @@ File name: ex1_is_even.cpp
 #include <iostream>
 using namespace std;
 
-//define function first
-void avg(int num1, int num2, int num3, int num4, int num5){
-    //finds the average between 5 numbers
-    double average = (num1 + num2 + num3 + num4 + num5) / 5.0;
-    cout << "The average of those marks is: " << average << endl;
-}
+void avg(int, int, int, int, int); //prototype
 
 int main(){
     int first, second, third, fourth, fifth; //declare vars
@@ -21,8 +16,15 @@ int main(){
     cin >> first >> second >> third >> fourth >> fifth; //initializes input into vars
 
     // Call the function
-    avg(first, second, third, fourth, fifth); //calls function
-    
+    avg(first, second, third, fourth, fifth); 
+
     return 0;
+}
+
+//define function avg
+void avg(int num1, int num2, int num3, int num4, int num5){
+    //finds the average between 5 numbers
+    double average = (num1 + num2 + num3 + num4 + num5) / 5.0;
+    cout << "The average of those marks is: " << average << endl;
 }
 
