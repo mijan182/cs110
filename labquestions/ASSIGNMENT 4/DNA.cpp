06608aeddf87be771d9d86ce1f697e7e777d0e76
mijan182 <jana-marie.reyes@rcsd.ca>
2025-11-26 15:06:08 -0600

    start of A4

---
+++ labquestions/ASSIGNMENT 4/DNA.cpp
@@ -0,0 +1,34 @@
+/*
+Name: Jana Marie Reyes
+ID: 200548478
+FILENAME: DNA.cpp
+DATE: 11/26/25
+*/
+
+
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+//function prototypes
+bool isSameGene(int);
+void get1Gene(int);
+void dnaAnalysis(int);
+
+
+int main(){
+
+}
+
+//function prototypes
+bool isSameGene(){
+
+}
+
+void get1Gene(){
+
+}
+
+void dnaAnalysis(){
+
+}
