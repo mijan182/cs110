f4e8397582f3368760f0ce4045b534fdc84db974
mijan182 <jana-marie.reyes@rcsd.ca>
2025-11-27 12:27:44 -0600

    yup

--- labquestions/ASSIGNMENT 4/DNA.cpp
+++ labquestions/ASSIGNMENT 4/DNA.cpp
@@ -10,14 +10,41 @@ DATE: 11/26/25
 #include <fstream>
 using namespace std;
 
+const int GENE_SIZE = 444;
+const int NUM_PPL = 4;
+const int GENE_PER_PPL = 2;
+
 //function prototypes
-bool isSameGene(int);
-void get1Gene(int);
-void dnaAnalysis(int);
+bool isSameGene(char gene1[], char gene2[], int size);
+void get1Gene(ifstream& input, char gene[], int size);
+string dnaAnalysis(char gene1[], char gene2[]);
 
 
 int main(){
+    ifstream input("dna.txt");
+    if (!input){
+        cout<<"Error: Could not open dna.txt" << endl;
+        return 1;
+    }
 
+    ofstream output("dnaAnalysis.txt");
+    if(!output){
+        cout << "Error: Could not create dnaAnalysis.txt" << endl;
+        return 1;
+    }
+
+    char genes[NUM_PPL][GENE_PER_PPL][GENE_SIZE];
+
+
+    for (int pereson = 0; person < NUM_PPL; person++){
+        for (int geneNum = 0; geneNum < GENE_PER_PPL; geneNum++){
+            get1Gene(input, genes[person][geneNum], GENE_SIZE);
+        }
+    }
+
+    input.close();
+
+    
 }
 
 //function prototypes
